578fc6c3fab4da35f89aa00e8915b37f5a1ff96b
beyza <beyza@DESKTOP-9NE921I>
2025-02-21 17:42:32 +0300

    First Commit

---
+++ src/main/java/com/project/springbootProject/model/User.java
@@ -0,0 +1,13 @@
+package com.project.springbootProject.model;
+
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Getter
+@Setter
+//@AllArgsConstructor
+//@NoArgsConstructor
+@Entity
+public class User {
+
+}
